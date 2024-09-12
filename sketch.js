4ed45ab515ba052de2ec95f8581b1714ecda765d
chamomilatea <cdelapuente21@gmail.com>
2024-09-12 10:46:18 -0500

    some other more recent changes

--- sketch.js
+++ sketch.js
@@ -1,25 +1,29 @@
 function setup() {
-    createCanvas(window.innerWidth, 1000);
-    background(0);
-  }
-  
-  function draw() {
-    background(0); // Clear the background each frame
-    addGrain(); // Add grain effect
-    circle(mouseX, mouseY, width * 0.01);
-  }
-  
-  function addGrain() {
-    loadPixels();
-    for (let x = 0; x < width; x++) {
-      for (let y = 0; y < height; y++) {
-        let index = (x + y * width) * 4;
-        let grain = random(0, 255);
-        pixels[index] = grain; // Red
-        pixels[index + 1] = grain; // Green
-        pixels[index + 2] = grain; // Blue
-        pixels[index + 3] = 50; // Alpha (transparency)
-      }
+  createCanvas(window.innerWidth, 1000);
+  background(0);
+}
+
+function draw() {
+  background(0); // Clear the background each frame
+  addGrain(); // Add grain effect
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  redraw(); // Redraw the canvas after resizing
+}
+
+function addGrain() {
+  loadPixels();
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let index = (x + y * width) * 4;
+      let grain = random(0, 225);
+      pixels[index] = grain; // Red
+      pixels[index + 1] = grain; // Green
+      pixels[index + 2] = grain; // Blue
+      pixels[index + 3] = 150; // Alpha (transparency)
     }
-    updatePixels();
   }
+  updatePixels();
+}

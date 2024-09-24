224d66cbe0bd212767fdd48a967d50f66c1523a4
chamomilatea <cdelapuente21@gmail.com>
2024-09-23 19:27:43 -0500

    yummy changes, question marks are wooorking

--- sketch.js
+++ sketch.js
@@ -1,15 +1,57 @@
+
+//SYD: just switch to p5.js for the rest of the class
+//We've suffered enough with HTML and CSS :)
+//Need preload function to load images
+//Design tip: I would make the transparency of grain effect lower
+let pageTwoImage;
+let pageTwoOn = false; // need this to toggle things on and off
+let button; //need to make this global cause we use it in draw & setup
+function preload(){
+
+  pageTwoImage = loadImage('Images/walkable.png');
+
+}
+
 function setup() {
+  
   createCanvas(windowWidth, windowHeight);
+  //Syd: added colorMode hue,sat,brightness
+  colorMode(HSB);
+
+  //SYD: I totally forgot about pixel density!!
+  //So you need this to set the density of your pixel to 1 because depending
+  //on what screen you view the piece on, the default pixel density will be different
+  //since you are using pixles to create your grain effect, you need this. 
+  //That's why it worked on VS Code and not on the browser.
+  pixelDensity(1); // Set the pixel density to 1 for better performance
+  
+  //Syd: I'll get you started with the button code and let you finish it
+  button = select("button");
+  button.mousePressed(pageTwo); //Create a page 2
+
+  background(0, 0, 0, 0.5); // Clear the background each frame
+
 }
 
 function draw() {
-  background(0, 0, 0, 0); // Clear the background each frame
+
+  if(!pageTwoOn){ //if page 2 is not on
   addGrain(); // Add grain effect
+  button.style('display', 'block') //this styles the button using p5 instead of css
+  
+  } else {
+    button.style('display', 'none') //hide the button
+  }
+
+
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  redraw(); // Redraw the canvas after resizing
+  addGrain(); // Add grain effect
+
+  //Syd:Not sure we need redraw here
+  //redraw(); // Redraw the canvas after resizing
 }
 
 function addGrain() {
@@ -17,12 +59,30 @@ function addGrain() {
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
       let index = (x + y * width) * 4;
-      let grain = random(0, 225);
+      let grain = random(0, 225); //black&white
       pixels[index] = grain; // Red
       pixels[index + 1] = grain; // Green
       pixels[index + 2] = grain; // Blue
-      pixels[index + 3] = 150; // Alpha (transparency)
+      pixels[index + 3] = 50; // Alpha (transparency)
     }
   }
   updatePixels();
 }
+
+function pageTwo() {
+  //Create a boolean to toggle things on and off (like the grain and button)
+  pageTwoOn = true;
+  //Syd: Display the image
+  image(pageTwoImage, 0, 0, width, height); //image, x,y,width,height
+  
+
+}
+
+// function changeImage() {
+//   const img = document.getElementById('background-image');
+//   img.style.opacity = 0; // Start fade out
+//   setTimeout(() => {
+//     img.src = 'Images/topdowngroceryselectyourgroceryitem.jpg'; // Change to the next image
+//     img.style.opacity = 1; // Fade in
+//   }, 500); // Adjust the timeout to match the transition duration
+// }
